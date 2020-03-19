388b427b44da7d54f0725225f9a1300df1069e8c
lizh <poinseting@163.com>
2020-03-19 23:17:16 +0800

    提交注释

--- Assets/SceneCanvas.cs
+++ Assets/SceneCanvas.cs
@@ -10,15 +10,24 @@ public class SceneCanvas : MonoBehaviour
     Canvas canvas;
     private void Awake()
     {
+        // 绑定场景GUI渲染回调
         // 2019之前用SceneView.onSceneGUIDelegate
         SceneView.duringSceneGui += OnSceneGUI;
+
+        // 修改Canvas的渲染模式
         canvas = GetComponent<Canvas>();
         canvas.renderMode = RenderMode.ScreenSpaceCamera;
         canvas.worldCamera = SceneView.lastActiveSceneView.camera;
     }
 
+    private void OnDestroy()
+    {
+        SceneView.duringSceneGui -= OnSceneGUI;
+    }
+
     private void OnSceneGUI(SceneView view)
     {
+        // 不知道为什么，需要每次都置空一下，才会成功修改Canvas的位置
         canvas.worldCamera = null;
         canvas.worldCamera = view.camera;
     }
