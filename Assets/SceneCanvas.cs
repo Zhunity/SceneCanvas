4de7479f48ff70df916c6345d326fa439d90219c
lizh <poinseting@163.com>
2020-03-19 22:54:04 +0800

    Scene Canvs

---
+++ Assets/SceneCanvas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Canvas))]
+public class SceneCanvas : MonoBehaviour
+{
+    Canvas canvas;
+    private void Awake()
+    {
+        SceneView.onSceneGUIDelegate += OnSceneGUI;
+        canvas = GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.worldCamera = SceneView.lastActiveSceneView.camera;
+    }
+
+    private void OnSceneGUI(SceneView view)
+    {
+        canvas.worldCamera = null;
+        canvas.worldCamera = view.camera;
+    }
+}
