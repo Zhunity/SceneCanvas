aeb698bc0d645d15658de0cfebdcb2ad1b02e820
lizh <poinseting@163.com>
2020-04-02 23:49:59 +0800

    屏蔽Scene操作

---
+++ Assets/EventExtension.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventExtension 
+{
+	public static bool IsLeftMouseButton(this Event e)
+	{
+		return e.isMouse && e.button == 0;
+	}
+
+	public static bool IsRightMouseButton(this Event e)
+	{
+		return e.isMouse && e.button == 1;
+	}
+
+	public static bool IsMiddleMouseButton(this Event e)
+	{
+		return e.isMouse && e.button == 2;
+	}
+}
--- Assets/SceneCanvas.cs
+++ Assets/SceneCanvas.cs
@@ -30,5 +30,37 @@ public class SceneCanvas : MonoBehaviour
         // 不知道为什么，需要每次都置空一下，才会成功修改Canvas的位置
         canvas.worldCamera = null;
         canvas.worldCamera = view.camera;
+
+        Event e = Event.current;
+        ///
+        /// 这里中间一系列操作
+        ///
+
+        ShieldSecneEvent(e);
+    }
+
+    private void ShieldSecneEvent(Event e)
+    {
+        // 屏蔽鼠标滚轮、右键、中间键
+        if(e.isScrollWheel || e.IsRightMouseButton() || e.IsMiddleMouseButton())
+        {
+            e.Use();
+        }
+
+        if (e.isKey)
+        {
+            // 屏蔽箭头操作
+            if(e.keyCode == KeyCode.DownArrow || e.keyCode == KeyCode.LeftArrow || e.keyCode == KeyCode.RightArrow || e.keyCode == KeyCode.UpArrow)
+            {
+                e.Use();
+            }
+        }
+
+        // 屏蔽左键操作导致的重绘
+        if(e.type == EventType.Layout)
+        {
+            int controlID = GUIUtility.GetControlID(FocusType.Passive);
+            HandleUtility.AddDefaultControl(controlID);
+        }
     }
 }
